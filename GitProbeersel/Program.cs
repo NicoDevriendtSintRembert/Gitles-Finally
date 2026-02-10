ce8ab20704166bdf7c73a6333086018ddfa0b9b8
Nico Devriendt <nico.devriendt@sint-rembert.be>
2026-02-10 11:57:05 +0100

    Update feestmenu

--- GitProbeersel/Program.cs
+++ GitProbeersel/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Nico Devriendt!");
-            Console.WriteLine("Ik eet graag frietjes met een bicky burger");
+            Console.WriteLine("Ik eet graag frietjes met een bicky burger en een boulet met yoppie saus");
         }
     }
 }
